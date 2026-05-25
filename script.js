e9eeb6d36427af8ec6aaa47a687a256b13a811d4
raaaaannnnn <144806067+raaaaannnnn@users.noreply.github.com>
2026-05-25 21:07:51 +0700

    Update script.js

--- script.js
+++ script.js
@@ -54,3 +54,98 @@ window.addEventListener('click', function(event) {
         sidebarToggle();
     }
 });
+
+document.getElementById('join-button').addEventListener('click', function() {
+    const nameError = document.getElementById('name-error');
+    const emailError = document.getElementById('email-error');
+    const phoneError = document.getElementById('phone-error');
+    const topicError = document.getElementById('topic-error');
+    const freqError = document.getElementById('freq-error');
+
+    nameError.innerText = "";
+    emailError.innerText = "";
+    phoneError.innerText = "";
+    topicError.innerText = "";
+    freqError.innerText = "";
+
+    const fullName = document.querySelector('.full-name input').value.trim();
+    const email = document.querySelector('.email input').value.trim();
+    const phone = document.querySelector('.phone-number input').value.trim();
+    const selectedTopic = document.querySelector('input[name="topic"]:checked');
+    const selectedFreq = document.getElementById('times-frequency').value;
+
+    let error = false;
+
+    if (fullName === "") {
+        nameError.innerText = "Name must be filled!";
+        error = true;
+    } else if (fullName.length < 3) {
+        nameError.innerText = "Name must be at least 3 characters!";
+        error = true;
+    } else if (fullName < 'A' || fullName > 'Z' && fullName < 'a' || fullName > 'z') {
+        nameError.innerText = "Name must be an alphabet!";
+        error = true;
+    } else if (fullName === " ") {
+        nameError.innerText = "Name can not contain space!";
+        error = true;
+    }
+
+    if (email === "") {
+        emailError.innerText = "Email must be filled!";
+        error = true;
+    } else if (!email.includes('@gmail.com')) {
+        emailError.innerText = "Email must be @gmail.com";
+        error = true;
+    } 
+
+    if (phone === "") {
+        phoneError.innerText = "Phone number must be filled!";
+        error = true;
+    } else if (phone.length < 10) {
+        phoneError.innerText = "Phone must be at least 10 numbers!";
+        error = true;
+    } else {
+        for (let i = 0; i < phone.length; i++) {
+            const char = phone[i];
+
+            if (i === 0 && char === '+') {
+                continue;
+            }
+
+            if (char === ' ') {
+                continue;
+            }
+
+            if (isNaN(char)) {
+                phoneError.innerText = "Phone number must be a numbers!";
+                error = true;
+                break;
+            }
+        }
+    }
+
+    if (!selectedTopic) {
+        topicError.innerText = "Topic must be selected at least one!";
+        error = true;
+    }
+
+    if (selectedFreq === "") {
+        freqError.innerText = "Freq must be selected at least one!";
+        error = true;
+    }
+    
+    if (error) return;
+
+    document.getElementById('newsletter').reset();
+});
+
+document.getElementById('scroll-to').addEventListener('click', function(){
+    const infoSection = document.querySelector('.main-information');
+
+    if (infoSection) {
+        infoSection.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    }
+});
